b3b554df06740abfd4f1defbfa2a5f03cb3a4421
Noah Iwasaki <iwasakin@mcmaster.ca>
2023-11-27 19:53:39 -0500

    worked on drawscreen

--- Project.cpp
+++ Project.cpp
@@ -6,6 +6,9 @@
 using namespace std;
 
 #define DELAY_CONST 100000 // 0.1s delay
+#define ROWS 10
+#define COLUMNS 20
+#define border BORSYM
 
 bool exitFlag;
 
@@ -43,7 +46,7 @@ void Initialize(void)
 
 void GetInput(void)
 {
-   
+
 }
 
 void RunLogic(void)
@@ -53,7 +56,22 @@ void RunLogic(void)
 
 void DrawScreen(void)
 {
-    MacUILib_clearScreen();    
+    MacUILib_clearScreen();
+
+    for (int i = 0; i < COLUMNS; i++)
+        cout << BORSYM;
+    cout << endl;
+    for (int i = 1; i < ROWS - 1; i++) {
+        cout << BORSYM
+        for (int j = 1; j < COLUMNS - 1; j++) {
+            cout << ' ';
+        }
+        cout << BORSYM << endl;
+    }
+    for (int i = 0; i < COLUMNS; i++)
+        cout << '#'
+    cout << endl;
+
 
 }
 
