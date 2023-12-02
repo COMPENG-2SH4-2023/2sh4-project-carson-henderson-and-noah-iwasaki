fb449a1b92a63c2039be8a34448d036a85bb09ac
Carson Henderson <hendec19@mcmaster.ca>
2023-12-02 14:12:18 -0500

    akdhskad

--- Player.cpp
+++ Player.cpp
@@ -7,6 +7,7 @@ Player::Player(GameMechs* thisGMRef)
     myDir = STOP;
 
     // more actions to be included
+    playerPosList = objPosArrayList();
 }
 
 
@@ -17,12 +18,57 @@ Player::~Player()
 
 void Player::getPlayerPos(objPos &returnPos)
 {
-    // return the reference to the playerPos arrray list
+    // return the reference to the playerPos array list
+    returnPos = &playerPosList;
 }
 
 void Player::updatePlayerDir()
 {
     // PPA3 input processing logic        
+    input = mainGameMechsRef.getInput();
+
+    if(input != 0)  // if not null character
+    {
+        switch(input)
+        {                      
+            case ' ':  // exit
+               mainGameMechsRef.exitFlag = 1;
+                break;
+
+            // direction processing
+            case 'w':
+                if(Dir != DOWN){
+                    Dir = UP;
+                }
+                break;
+
+            case 's':
+                if(Dir != UP){
+                    Dir = DOWN;
+                }
+                break;
+
+            case 'a':
+                if(Dir != RIGHT){
+                    Dir = LEFT;
+                }
+                break;
+
+            case 'd':
+                if(Dir != LEFT){
+                    Dir = RIGHT;
+                }
+                break;
+
+            default:
+                break;
+        }
+        input = 0;
+    }
+
+
+
+
 }
 
 void Player::movePlayer()
--- objPosArrayList.cpp
+++ objPosArrayList.cpp
@@ -1,5 +1,95 @@
 #include "objPosArrayList.h"
+#include "objPos.h"
 
-// Paste your CUTE Tested implementation here.
-// Paste your CUTE Tested implementation here.
-// Paste your CUTE Tested implementation here.
+
+// Check lecture contents on general purpose array list construction, 
+// and modify it to support objPos array list construction.
+
+objPosArrayList::objPosArrayList()
+{
+
+    sizeArray = ARRAY_MAX_CAP;
+    sizeList = 0;
+
+    aList = new objPos[sizeArray];
+}
+
+objPosArrayList::~objPosArrayList()
+{
+    delete[] aList;
+}
+
+int objPosArrayList::getSize()
+{
+    return sizeList;
+}
+
+void objPosArrayList::insertHead(objPos thisPos)
+{
+
+    // check if arraylist is full
+    if (sizeList == sizeArray){
+        return;
+    }
+
+    // move everything back one, starting at the end
+    for (int i = sizeList; i > 0; i--){
+        aList[i].setObjPos(aList[i - 1]);
+    }
+
+    // insert a new element at the head
+    aList[0].setObjPos(thisPos);
+    sizeList++;
+}
+
+void objPosArrayList::insertTail(objPos thisPos)
+{
+    // check if arraylist is full
+    if (sizeList == sizeArray){
+        return;
+    }
+
+    // add the input to the end of the list
+    aList[sizeList].setObjPos(thisPos);
+    sizeList++;
+}
+
+void objPosArrayList::removeHead()
+{
+    // check if arraylist is full
+    if (sizeList == sizeArray){
+        return;
+    }
+
+    // move every element forward by 1
+    for (int i = --sizeList; i > 0; i++){
+        aList[i].setObjPos(aList[i+1]);
+    }
+}
+
+void objPosArrayList::removeTail()
+{
+    // if list is not empty remove the tail
+    if (sizeList > 0){
+        sizeList--;
+    }
+}
+
+void objPosArrayList::getHeadElement(objPos &returnPos)
+{
+    // check if list is full
+    returnPos.setObjPos(aList[0]);
+}
+
+void objPosArrayList::getTailElement(objPos &returnPos)
+{
+    returnPos.setObjPos(aList[sizeList - 1]);
+}
+
+void objPosArrayList::getElement(objPos &returnPos, int index)
+{
+    if (index >= sizeList){
+        index = sizeList - 1;
+    }
+    returnPos.setObjPos(aList[index]);
+}
